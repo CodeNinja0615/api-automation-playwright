7ba51bfc82ca8adefa77c9f5a87d5ed270bca664
CodeNinja0615 <akhtarsameer743@gmail.com>
2026-02-21 00:35:29 +0530

    Test Herokupapp API Testing Day11:  Splitting test with test.step for better clarity

--- tests/api/createBooking.e2e.spec.js
+++ tests/api/createBooking.e2e.spec.js
@@ -4,9 +4,8 @@ import { bookingSchema } from '../../src/api/schemas/booking.schema.js';
 import { validateSchema } from '../../src/utils/schemaValidator.js';
 
 test('Create → Get → Delete booking (E2E)', async ({ request, authToken }) => {
-
-  const bookingService = new BookingService(request, authToken);
-
+  /** @type {BookingService} */
+  let bookingService, bookingId;
   const payload = {
     firstname: "Sameer",
     lastname: "Akhtar",
@@ -18,23 +17,27 @@ test('Create → Get → Delete booking (E2E)', async ({ request, authToken }) =
     },
     additionalneeds: "Breakfast"
   };
+  await test.step(`Create Booking`, async () => {
+    bookingService = new BookingService(request, authToken);
+    // 1️⃣ Create
+    const createResponse = await bookingService.createBooking(payload);
+    expect(createResponse.status()).toBe(200);
 
-  // 1️⃣ Create
-  const createResponse = await bookingService.createBooking(payload);
-  expect(createResponse.status()).toBe(200);
+    const createBody = await createResponse.json();
+    bookingId = createBody.bookingid;
+  });
+  await test.step(`Get Booking`, async () => {
+    // 2️⃣ Get
+    const getResponse = await bookingService.getBookingById(bookingId);
+    expect(getResponse.status()).toBe(200);
 
-  const createBody = await createResponse.json();
-  const bookingId = createBody.bookingid;
-
-  // 2️⃣ Get
-  const getResponse = await bookingService.getBookingById(bookingId);
-  expect(getResponse.status()).toBe(200);
-
-  const getBody = await getResponse.json();
-  expect(getBody.firstname).toBe("Sameer");
-  expect(validateSchema(bookingSchema, getBody)).toBeTruthy(); //----Response schema validation
-
-  // 3️⃣ Delete
-  const deleteResponse = await bookingService.deleteBooking(bookingId);
-  expect(deleteResponse.status()).toBe(201);
+    const getBody = await getResponse.json();
+    expect(getBody.firstname).toBe("Sameer");
+    expect(validateSchema(bookingSchema, getBody)).toBeTruthy(); //----Response schema validation
+  });
+  await test.step(`Delete Booking`, async () => {
+    // 3️⃣ Delete
+    const deleteResponse = await bookingService.deleteBooking(bookingId);
+    expect(deleteResponse.status()).toBe(201);
+  });
 });
